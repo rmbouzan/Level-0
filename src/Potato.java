b386d7b031fd9b33f54f5f60082d28a9a8b01b02
rmbouzan <Guest@wts6.attlocal.net>
2015-10-06 18:33:54 -0700

    stuff

---
+++ src/Potato.java
@@ -0,0 +1,20 @@
+public class Potato {
+	public static void main(String[] args) {
+		int verse = 1;
+		for (int i = 1; i < 9; i++) {
+			if (i == 1) {
+
+			}
+			if(i == 5){
+				System.out.println("4");
+			}
+			else if ( i == 8) {
+				System.out.println("more");
+
+			} else {
+				System.out.println(i + " potato");
+			}
+
+		}
+	}
+}
